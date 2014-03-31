0e657779d54685ee489dc2a511e3698c4b6c46e2
Jeremy Clemans & John Cech <student@epicodus.com>
2014-03-31 12:29:37 -0700

    initial file setup

---
+++ to_do_manager/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToDoManager::Application.config.secret_key_base = 'bb2cf2afa5775bc43f19c142ced58d25e94d42b9aaffbab69f195f3cb13e1460b16651389774c861d6d6590b20f4450f898de445aaa826a2ceb5ae94012a387d'
