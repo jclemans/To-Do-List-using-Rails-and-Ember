8bc5a53d7b95ae4688a211d5fab7326f1d512dcc
Jeremy <student@epicodus.com>
2014-05-07 09:24:53 -0700

    add readme description

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+    @task = Task.new
+    render('tasks/index.html.erb')
+  end
+
+  def create
+    @task = Task.new(:name => params[:task],
+                     :done => false)
+    if @task.save
+      redirect_to '/'
+    else
+      render('tasks/index.html.erb')
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render('tasks/show.html.erb')
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to '/tasks'
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    render('tasks/edit.html.erb')
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(:name => params[:task])
+      redirect_to '/'
+    else
+      render('tasks/edit.html.erb')
+    end
+  end
+
+  def mark_done
+    @task = Task.find(params[:id])
+    @task.update(:done => true)
+    redirect_to '/'
+  end
+end
+
